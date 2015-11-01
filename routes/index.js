5848d44f180d9e061b782867a34b0371dfde0759
peeweemiwie <miwayagi@gmail.com>
2015-11-01 09:38:04 -0500

    Add gitignore. Reorganize file structure.

--- routes/index.js
+++ routes/index.js
@@ -3,7 +3,7 @@ var router = express.Router();
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Codepan test' });
+  res.render('index', { title: 'Codepen test' });
 });
 
 router.get('/animation', function(req, res, next) {
