8c464278d041638d7c1777be4c9f36276eeb6595
peeweemiwie <miwayagi@gmail.com>
2015-10-31 22:34:20 -0400

    Concatenate js file.

--- gulpfile.js
+++ gulpfile.js
@@ -1,7 +1,15 @@
 var gulp = require('gulp'),
-    gutil = require('gulp-util');
+    gutil = require('gulp-util'),
+    concat = require('gulp-concat');
+
+var jsSources = [
+  'components/scripts/script.js'
+];
 
 
-gulp.task('log', function(){
+gulp.task('js', function(){
   gutil.log('hello world!');
+  gulp.src(jsSources)
+    .pipe(concat('script.js'))
+    .pipe(gulp.dest('builds/development/js'))
 });
