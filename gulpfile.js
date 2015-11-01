4218e6ca976f7d302897c6f4b459ad63b1549071
peeweemiwie <miwayagi@gmail.com>
2015-10-31 23:17:14 -0400

    Add compass and sass.

--- gulpfile.js
+++ gulpfile.js
@@ -1,15 +1,28 @@
 var gulp = require('gulp'),
     gutil = require('gulp-util'),
+    compass = require('gulp-compass'),
     concat = require('gulp-concat');
 
 var jsSources = [
   'components/scripts/script.js'
 ];
 
+var sassSources = ['components/sass/style.scss']
 
 gulp.task('js', function(){
   gutil.log('hello world!');
   gulp.src(jsSources)
     .pipe(concat('script.js'))
     .pipe(gulp.dest('builds/development/js'))
+});
+
+gulp.task('compass', function(){
+  gulp.src(sassSources)
+    .pipe(compass({
+      sass: 'components/sass',
+      image: 'builds/development/images',
+      style: 'expanded'
+    }))
+    .on('error', gutil.log)
+    .pipe(gulp.dest('builds/development/css'))
 });
